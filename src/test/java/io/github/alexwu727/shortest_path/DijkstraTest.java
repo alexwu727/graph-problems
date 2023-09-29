12d6d336d7a39431e72655d118709be90db25da0
Alex Wu <alexwu727@gmail.com>
2023-09-29 00:34:24 -0700

    add another test case

--- src/test/java/io/github/alexwu727/shortest_path/DijkstraTest.java
+++ src/test/java/io/github/alexwu727/shortest_path/DijkstraTest.java
@@ -12,16 +12,10 @@ import java.util.HashMap;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DijkstraTest {
+    private final Graph graph = new Graph();
 
-    @Test
-    void dijkstra() {
-        Graph graph = new Graph();
-        Node node0 = new Node(0);
-        Node node1 = new Node(1);
-        Node node2 = new Node(2);
-        Node node3 = new Node(3);
-        Node node4 = new Node(4);
-
+    @BeforeEach
+    public void setUp() {
         ArrayList<Edge> edges = new ArrayList<>();
         edges.add(new Edge(0, 1, 5));
         edges.add(new Edge(0, 3, 10));
@@ -30,14 +24,27 @@ class DijkstraTest {
         edges.add(new Edge(1, 4, 3));
         edges.add(new Edge(2, 3, 15));
         edges.add(new Edge(3, 4, 2));
-
-
         graph.addEdges(edges);
+    }
+
+    @Test
+    void dijkstra_StartWithNode0() {
         HashMap<String, int[]> res = Dijkstra.dijkstra(graph, 0);
         int[] dist = res.get("dist");
         int[] prev = res.get("prev");
         int[] expectedDist = new int[] {0, 5, 24, 9, 7};
         int[] expectedPrev = new int[] {-1, 0, 3, 4, 0};
+        assertArrayEquals(expectedDist, dist);
+        assertArrayEquals(expectedPrev, prev);
+    }
+
+    @Test
+    void dijkstra_StartWithNode2() {
+        HashMap<String, int[]> res = Dijkstra.dijkstra(graph, 2);
+        int[] dist = res.get("dist");
+        int[] prev = res.get("prev");
+        int[] expectedDist = new int[] {24, 20, 0, 15, 17};
+        int[] expectedPrev = new int[] {4, 4, -1, 2, 3};
         assertArrayEquals(expectedDist, dist);
         assertArrayEquals(expectedPrev, prev);
     }
