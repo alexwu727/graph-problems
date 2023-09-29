9e36c7aefe1dfd9109647f92de6a831140deb6fc
Alex Wu <alexwu727@gmail.com>
2023-09-29 00:29:55 -0700

    implement bellman ford algorithm

--- src/main/java/io/github/alexwu727/shortest_path/BellmanFord.java
+++ src/main/java/io/github/alexwu727/shortest_path/BellmanFord.java
@@ -1,2 +1,41 @@
-package io.github.alexwu727.shortest_path;public class BellmanFord {
+package io.github.alexwu727.shortest_path;
+
+import io.github.alexwu727.utils.Edge;
+import io.github.alexwu727.utils.Graph;
+import io.github.alexwu727.utils.Printer;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class BellmanFord {
+    public static HashMap<String, int[]> bellmanFord(Graph graph, int startNode) {
+        int n = graph.getNumNodes();
+        HashMap<String, int[]> res = new HashMap<>();
+        int[] dist = new int[n];
+        int[] prev = new int[n];
+        res.put("dist", dist);
+        res.put("prev", prev);
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        Arrays.fill(prev, -1);
+        dist[startNode] = 0;
+        for (int i = 0; i < n; i++) {
+            boolean updated = false;
+            for (int curr = 0; curr < n; curr++) {
+                if (dist[curr] == Integer.MAX_VALUE) continue;
+                // update dist
+                for (Edge edge : graph.getOutDegreeEdges(curr)) {
+                    int nei = edge.getDest();
+                    if (dist[nei] > dist[curr] + edge.getWeight()) {
+                        dist[nei] = dist[curr] + edge.getWeight();
+                        prev[nei] = curr;
+                        updated = true;
+                    }
+                }
+            }
+//            Printer.printArray("dist", dist);
+            if (!updated) break;
+        }
+
+        return res;
+    }
 }
--- src/test/java/io/github/alexwu727/shortest_path/BellmanFordTest.java
+++ src/test/java/io/github/alexwu727/shortest_path/BellmanFordTest.java
@@ -1,4 +1,85 @@
+package io.github.alexwu727.shortest_path;
+
+import io.github.alexwu727.utils.Edge;
+import io.github.alexwu727.utils.Graph;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BellmanFordTest {
-  
+    private final Graph graphWithoutNegWeight = new Graph();
+    private final Graph graphWithNegWeight = new Graph();
+
+    @BeforeEach
+    public void setUp() {
+        ArrayList<Edge> edges1 = new ArrayList<>();
+        edges1.add(new Edge(0, 1, 4));
+        edges1.add(new Edge(0, 3, 7));
+        edges1.add(new Edge(0, 4, 17));
+        edges1.add(new Edge(1, 2, 12));
+        edges1.add(new Edge(1, 4, 8));
+        edges1.add(new Edge(2, 3, 6));
+        edges1.add(new Edge(3, 4, 2));
+        edges1.add(new Edge(4, 2, 4));
+        graphWithoutNegWeight.addEdges(edges1);
+
+        ArrayList<Edge> edges2 = new ArrayList<>();
+        edges2.add(new Edge(0, 1, 10));
+        edges2.add(new Edge(0, 5, 8));
+        edges2.add(new Edge(1, 3, 2));
+        edges2.add(new Edge(2, 1, 1));
+        edges2.add(new Edge(3, 2, -2));
+        edges2.add(new Edge(4, 1, -4));
+        edges2.add(new Edge(4, 3, -1));
+        edges2.add(new Edge(5, 4, 1));
+        graphWithNegWeight.addEdges(edges2);
+    }
+
+    @Test
+    void bellmanFord_OnGraphWithoutNegWeight_StartWithNode0() {
+        HashMap<String, int[]> res = BellmanFord.bellmanFord(graphWithoutNegWeight, 0);
+        int[] dist = res.get("dist");
+        int[] prev = res.get("prev");
+        int[] expectedDist = new int[] {0, 4, 13, 7, 9};
+        int[] expectedPrev = new int[] {-1, 0, 4, 0, 3};
+        assertArrayEquals(expectedDist, dist);
+        assertArrayEquals(expectedPrev, prev);
+    }
+
+    @Test
+    void bellmanFord_OnGraphWithoutNegWeight_StartWithNode2() {
+        HashMap<String, int[]> res = BellmanFord.bellmanFord(graphWithoutNegWeight, 2);
+        int[] dist = res.get("dist");
+        int[] prev = res.get("prev");
+        int[] expectedDist = new int[] {Integer.MAX_VALUE, Integer.MAX_VALUE, 0, 6, 8};
+        int[] expectedPrev = new int[] {-1, -1, -1, 2, 3};
+        assertArrayEquals(expectedDist, dist);
+        assertArrayEquals(expectedPrev, prev);
+    }
+
+    @Test
+    void bellmanFord_OnGraphWithNegWeight_StartWithNode0() {
+        HashMap<String, int[]> res = BellmanFord.bellmanFord(graphWithNegWeight, 0);
+        int[] dist = res.get("dist");
+        int[] prev = res.get("prev");
+        int[] expectedDist = new int[] {0, 5, 5, 7, 9, 8};
+        int[] expectedPrev = new int[] {-1, 4, 3, 1, 5, 0};
+        assertArrayEquals(expectedDist, dist);
+        assertArrayEquals(expectedPrev, prev);
+    }
+
+    @Test
+    void bellmanFord_OnGraphWithNegWeight_StartWithNode2() {
+        HashMap<String, int[]> res = BellmanFord.bellmanFord(graphWithNegWeight, 2);
+        int[] dist = res.get("dist");
+        int[] prev = res.get("prev");
+        int[] expectedDist = new int[] {Integer.MAX_VALUE, 1, 0, 3, Integer.MAX_VALUE, Integer.MAX_VALUE};
+        int[] expectedPrev = new int[] {-1, 2, -1, 1, -1, -1};
+        assertArrayEquals(expectedDist, dist);
+        assertArrayEquals(expectedPrev, prev);
+    }
 }
